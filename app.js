27de3168adecaef7781a294cb7bfd4106d041a09
Filip Herbst <filip_herbst@yahoo.com>
2024-09-23 11:05:37 -0500

    JS Variables Exercise Completed

--- app.js
+++ app.js
@@ -4,7 +4,55 @@ console.log(
 );
 
 // Exercise 1
+let lastName = "";
+var age = "";
 
 // Exercise 2
+const firstName = "Filip";
+lastName = "Herbst";
+age = 41;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+
+console.log(`Exercise 2:
+  First Name: ${firstName}
+  Last Name: ${lastName}
+  Age: ${age}`);
+
+console.log(`Exercise 3:
+  Language: ${language}
+  Year Created: ${createdYear}
+  Case Sensitive: ${isCaseSensitive}`);
+
+console.log(`Exercise 4:
+  Price: ${price}
+  On Sale: ${isOnSale}
+  Percent Off When on Sale: ${salePercentage}%
+  Number in Stock: ${stock}
+  Currently in Stock: ${inStock}
+  Popular Size: ${selectedSize}`);
+
+console.log(`Exercise 5:
+  Title of Book: ${title}
+  Author: ${author}
+  Page Count: ${pageCount}
+  Book Mark: ${bookMark}
+  Have you read it? : ${hasRead}`);
